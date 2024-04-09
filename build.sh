4d29b421638273ec4d56090a84ff43320a850847
Achim Friedland <achim.friedland@graphdefined.com>
2024-04-09 03:59:33 +0200

    Fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-#tsc -p tsconfig.json
 sass src/css/chargy.scss src/css/chargy.css
-webpack -c webpack.config.cjs
-
-electron-builder
+webpack -c webpack.config-web.cjs
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#tsc -p tsconfig.json
-sass src/css/chargy.scss src/css/chargy.css
-webpack -c webpack.config.cjs
-
-npm start --silent -- --inspect "$@"
