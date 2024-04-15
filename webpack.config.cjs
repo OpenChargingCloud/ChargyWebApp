e9f4d7009ed323a36011b85cb8e2cb01c070b093
Achim Friedland <achim.friedland@graphdefined.com>
2024-04-15 03:22:44 +0200

    Fix webpack for dev server

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,52 +1,74 @@
-const webpack = require('webpack');
-const path    = require('path');
+const path              = require('path');
+const webpack           = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports =
-    {
-      mode:    'development',
-      entry:   './src/ts/chargyApp.ts',
-      target:  'web',
-      devtool: "eval-source-map",  // Do not use in production!
-      //devtool: "source-map",     // Secure, but very slow: Use in production!
-      resolve: {
-        extensions: [".ts", ".js"],
-        fallback: {
-          "fs":           false,
-          "original-fs":  false,
-          "path":         require.resolve("path-browserify"),
-          "http":         require.resolve("stream-http"),
-          "crypto":       require.resolve("crypto-browserify"),
-          "url":          require.resolve("url/"),
-          "stream":       require.resolve("stream-browserify"),
-          "vm":           require.resolve("vm-browserify"),
-          "buffer":       require.resolve("buffer/"),
-          "node:buffer":  require.resolve("buffer/")
+module.exports = {
+  mode:    'development',
+  entry:   './src/ts/chargyApp.ts',
+  target:  'web',
+  devtool: "eval-source-map",  // Do not use in production!
+  //devtool: "source-map",     // Secure, but very slow: Use in production!
+  resolve: {
+    extensions: [".ts", ".js"],
+    fallback: {
+      "fs":           false,
+      "original-fs":  false,
+      "path":         require.resolve("path-browserify"),
+      "http":         require.resolve("stream-http"),
+      "crypto":       require.resolve("crypto-browserify"),
+      "url":          require.resolve("url/"),
+      "stream":       require.resolve("stream-browserify"),
+      "vm":           require.resolve("vm-browserify"),
+      "buffer":       require.resolve("buffer/"),
+      "node:buffer":  require.resolve("buffer/")
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      include: path.resolve(__dirname, 'src/ts'),
+      use: [{ loader: 'ts-loader' }]
+    }]
+  },
+  externals: {
+    'asn1':         'asn1.js',
+    'base32decode': 'base32-decode'
+  },
+  output: {
+    path:     path.resolve(__dirname, 'build'),
+    filename: 'chargyWebApp-bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'html/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'html/css'),
+          to:   path.resolve(__dirname, 'build/css')
+        },
+        {
+          from: path.resolve(__dirname, 'html/images'),
+          to:   path.resolve(__dirname, 'build/images')
+        },
+        {
+          from: path.resolve(__dirname, 'html/webfonts'),
+          to:   path.resolve(__dirname, 'build/webfonts')
         }
-      },
-      module: {
-        rules: [{
-          test: /\.ts$/,
-          include: path.resolve(__dirname, 'src/ts'),
-          use: [{ loader: 'ts-loader' }]
-        }]
-      },
-      externals: {
-        'asn1':         'asn1.js',
-        'base32decode': 'base32-decode'
-      },
-      output: {
-        path: __dirname + '/build',
-        filename: 'chargyWebApp-bundle.js'
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-        }),
-      ],
-      devServer: {
-        host: 'localhost',
-        port:  1608,
-        hot:   true,
-        open:  true
-      }
-    };
+      ]
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'html'),
+    },
+    port:  1608,
+    hot:   true,
+    open:  true
+  }
+};
