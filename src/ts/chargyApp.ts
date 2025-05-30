c3e1ed653f55626a6cbbe09b2d84fe1bb12ecc13
Achim Friedland <achim.friedland@graphdefined.com>
2025-05-30 06:13:53 +0200

    Fix build

--- src/ts/chargyApp.ts
+++ src/ts/chargyApp.ts
@@ -620,13 +620,14 @@ export class ChargyApp {
         this.map   = L.map(document.getElementById('map') as HTMLElement);
         this.map.setView([49.7325504, 10.1424442], 10);
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        const accessToken = 'pk.eyJ1IjoiYWh6ZiIsImEiOiJOdEQtTkcwIn0.Cn0iGqUYyA6KPS8iVjN68w';
+
+        L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`, {
             attribution:  '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
             tileSize:      512,
             maxZoom:        18,
             zoomOffset:     -1,
             id:           'mapbox/light-v10'
-            //accessToken:  'pk.eyJ1IjoiYWh6ZiIsImEiOiJOdEQtTkcwIn0.Cn0iGqUYyA6KPS8iVjN68w'
         }).addTo(this.map);
 
 
